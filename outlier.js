41ef4b426bc1fcd4808a36feb58c2a568e73bb1c
Laura Wu <leemaiwu@gmail.com>
2023-03-13 10:10:39 -0600

    Completed morning challenge.

---
+++ outlier.js
@@ -0,0 +1,20 @@
+//let array = [2, 4, 0, 100, 4, 11, 2602, 36]
+let array = [160, 3, 1719, 19, 11, 13, -21]
+
+function outlier () {
+    evenOutput = []
+    oddOutput = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 !== 0) {
+            evenOutput.push(array[i])
+        } else if (array[i] % 3 !== 0) {
+            oddOutput.push(array[i])
+        }
+    } if (evenOutput < oddOutput) {
+        return evenOutput
+    } else if (oddOutput < evenOutput) {
+        return oddOutput
+    }
+}
+
+console.log(outlier(array))
